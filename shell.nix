2c6ae3c54548c703749055b834ac853107fdfb9e
thiagokoster <thiagokoster@gmail.com>
2024-11-13 21:22:17 +0100

    add shell.nix

---
+++ shell.nix
@@ -0,0 +1,20 @@
+let 
+	pkgs = import <nixpkgs> {};
+in
+	pkgs.mkShell {
+		packages = [
+			pkgs.cargo
+			pkgs.rustc
+
+			pkgs.rust-analyzer
+			pkgs.rustfmt
+
+			# If the dependencies need system libs, you usually need pkg-config + the lib
+			pkgs.pkg-config
+			pkgs.openssl
+		];
+
+		env = {
+			RUST_BACKTRACE = "full";
+		};
+	}
